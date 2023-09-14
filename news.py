980eed89d9802c14b7e0919649a05586d5429ea1
Robert Horvat <robert.horvath93@gmail.com>
2023-09-15 00:28:18 +0300

    Improvement : english only, links, max 20 news, subject in mail

--- news.py
+++ news.py
@@ -1,10 +1,15 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
+# tesla was hardcoded into the url
+# we replaced it with the variable inside the url
+
 api_key = "a693352cfdda4fe78f4cc555a67c4ba1"
-url = "https://newsapi.org/v2/everything?q=tesla" \
-      "&from=2023-08-14&sortBy=publishedAt&apiKey" \
-      "=a693352cfdda4fe78f4cc555a67c4ba1"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&from=2023-08-14&sortBy=publishedAt&apiKey" \
+      "=a693352cfdda4fe78f4cc555a67c4ba1&language=en"
+# added &language=en at end of url to display only english pages
 # we broke the string with \
 # url = "https://finance.yahoo.com"
 
@@ -18,12 +23,13 @@ content = request_url.json()
 # json because we need a dictionary format
 # Access the article titles and description
 
-body = ""
-for article in content["articles"]:
+body = "Subject: Today's news" + "\n"
+for article in content["articles"][:20]:
+                        # indexed up to 20 news on mail
       if article["title"] is not None:
-            body = body + article["title"] + "\n"
+            body += article["title"] + "\n"
       if article["description"] is not None:
-            body = body + article["description"] + 2*"\n"
+            body += article["description"] + "\n" + article["url"] + 2*"\n"
 
 
 body = body.encode("utf-8")
